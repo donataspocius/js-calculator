851d18490237b8f7694ba06d88456c6e0c659583
Donatas Pocius <pocius.coding@gmail.com>
2022-08-01 21:14:26 +0300

    Finished Task v2

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 "use strict";
 
-let sumBtn = document.querySelector("#sumBtn");
-let subBtn = document.querySelector("#subBtn");
-let divBtn = document.querySelector("#divBtn");
-let mulBtn = document.querySelector("#mulBtn");
-let modBtn = document.querySelector("#modBtn");
-let clearBtn = document.querySelector("#clearBtn");
-let output = document.querySelector("#output");
-let btns = document.querySelectorAll(".btn");
-
-let num1 = document.querySelector("#num1");
-let num2 = document.querySelector("#num2");
-
 btns.forEach((btn) =>
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
     let number1 = Number(num1.value);
     let number2 = Number(num2.value);
 
-    switch (btn.textContent) {
+    switch (e.target.textContent) {
       case "SUM": {
         return (output.textContent = number1 + number2);
       }
@@ -33,41 +21,9 @@ btns.forEach((btn) =>
       case "MOD": {
         return (output.textContent = number1 % number2);
       }
+      case "CLEAR": {
+        return (output.textContent = ""), (num1.value = num2.value = "");
+      }
     }
   })
 );
-
-clearBtn.addEventListener("click", function () {
-  num1.value = num2.value = "";
-  output.textContent = "";
-});
-
-// sumBtn.addEventListener("click", function () {
-//   let number1 = Number(num1.value);
-//   let number2 = Number(num2.value);
-//   return (output.textContent = number1 + number2);
-// });
-
-// subBtn.addEventListener("click", function () {
-//   let number1 = Number(num1.value);
-//   let number2 = Number(num2.value);
-//   return (output.textContent = number1 - number2);
-// });
-
-// divBtn.addEventListener("click", function () {
-//   let number1 = Number(num1.value);
-//   let number2 = Number(num2.value);
-//   return (output.textContent = number1 / number2);
-// });
-
-// mulBtn.addEventListener("click", function () {
-//   let number1 = Number(num1.value);
-//   let number2 = Number(num2.value);
-//   return (output.textContent = number1 * number2);
-// });
-
-// modBtn.addEventListener("click", function () {
-//   let number1 = Number(num1.value);
-//   let number2 = Number(num2.value);
-//   return (output.textContent = number1 % number2);
-// });
